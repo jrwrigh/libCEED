86acd95f322788443029fa74804287cdc83d4f2a
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2019-10-25 17:46:39 -0600

    a bug fixed

--- examples/navier-stokes/advection.h
+++ examples/navier-stokes/advection.h
@@ -244,7 +244,7 @@ CEED_QFUNCTION(Advection)(void *ctx, CeedInt Q,
     // Stabilization requires a measure of element transit time in the velocity
     // field u.
     CeedScalar uX[3];
-    for (int j=0; j<3; j++) uX[j] = dXdx[j][0]*u[0] + dXdx[j][1]*u[1];
+    for (int j=0; j<3; j++) uX[j] = dXdx[j][0]*u[0] + dXdx[j][1]*u[1] + dXdx[j][2]*u[2];
     const CeedScalar TauS = CtauS / sqrt(uX[0]*uX[0] + uX[1]*uX[1] + uX[2]*uX[2]);
     for (int j=0; j<3; j++)
       dv[j][4][i] -= wJ * TauS * strongConv * uX[j];
