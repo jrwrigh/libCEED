556c857e1e9a4ba253c29e21068bf2145e3c9e24
valeriabarra <valeriabarra21@gmail.com>
2019-11-06 08:47:36 -0700

    Update test t000-ceed.py using opaque pointer

--- interface/ceed-python/t000-ceed.py
+++ interface/ceed-python/t000-ceed.py
@@ -1,7 +1,6 @@
 from _ceed import ffi, lib
 
 if __name__ == "__main__":
-  ceed = ffi.new("Ceed")
-  lib.CeedInit("cpu/self/ref/serial", ceed)
-  print(ceed)
-  print(ceed[0])
+  ceed = ffi.new("Ceed *")
+  resource = ffi.new("char[]", "/cpu/self".encode('ascii'))
+  lib.CeedInit(resource, ceed)
