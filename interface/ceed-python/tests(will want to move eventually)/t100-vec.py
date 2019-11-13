c4852340bb5ed2bf644cb4647dae75c39ae490d1
valeriabarra <valeriabarra21@gmail.com>
2019-11-12 23:18:31 -0700

    Rename vector->x in t100-vec.py

--- interface/ceed-python/tests(will want to move eventually)/t100-vec.py
+++ interface/ceed-python/tests(will want to move eventually)/t100-vec.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
   ceed = libceed.ceed(sys.argv[1])
 
   n = 10
-  vector = ceed.vector(n)
+  x = ceed.vector(n)
 
   a = np.arange(10, 10 + n, dtype="float64")
-  vector.setArray(mem_host, use_pointer, a)
+  x.setArray(mem_host, use_pointer, a)
 
-  b = vector.getArrayRead(mem_host)
+  b = x.getArrayRead(mem_host)
   for i in range(n):
     if b[i] != 10 + i:
       # LCOV_EXCL_START
       print("Error reading array b[%d] = %4.2f" %(i, b[i]))
       # LCOV_EXCL_STOP
 
-  vector.restoreArrayRead()
+  x.restoreArrayRead()
