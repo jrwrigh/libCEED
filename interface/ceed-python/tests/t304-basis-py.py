67eb59d10083de03a800defae7192ff047923240
valeriabarra <valeriabarra21@gmail.com>
2019-11-15 13:19:56 -0700

    Complete t304-basis-py.py

--- interface/ceed-python/tests/t304-basis-py.py
+++ interface/ceed-python/tests/t304-basis-py.py
@@ -14,4 +14,17 @@ if __name__ == "__main__":
                 0.0332866, -0.07448852, 0.0745459, 0.19996678], dtype="float64")
   lam = np.empty(4, dtype="float64")
 
-  libceed.Basis.symmetric_schur_decomposition(ceed, A, 4)
+  lam = libceed.Basis.symmetric_schur_decomposition(ceed, A, 4)
+
+  print("Q: ")
+  for i in range(4):
+    for j in range(4):
+      if A[j+4*i] <= 1E-14 and A[j+4*i] >= -1E-14:
+         A[j+4*i] = 0
+      print("%12.8f"%A[j+4*i])
+
+  print("lmbda: ")
+  for i in range(4):
+    if lam[i] <= 1E-14 and lam[i] >= -1E-14:
+      lam[i] = 0
+    print("%12.8f"%lam[i])
