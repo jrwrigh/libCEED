e64e6e98097e6dfe412413db14a21fc568b28348
valeriabarra <valeriabarra21@gmail.com>
2019-11-15 14:27:50 -0700

    Simplify assignments in t303-basis-py.py

--- interface/ceed-python/tests/t303-basis-py.py
+++ interface/ceed-python/tests/t303-basis-py.py
@@ -8,10 +8,7 @@ import numpy as np
 if __name__ == "__main__":
   ceed = libceed.Ceed(sys.argv[1])
 
-  Q = 8
-  P = 2
-  dim = 3
-  ncomp = 1
+  Q, P, dim, ncomp = 8, 2, 3, 1
   length = Q**dim
   U = ceed.Vector(length)
   V = ceed.Vector(length + 1)
@@ -19,4 +16,4 @@ if __name__ == "__main__":
   b = ceed.BasisTensorH1Lagrange(dim, ncomp, P, Q, libceed.GAUSS)
 
   # Basis apply will error because dimensions don't agree
-  b.apply(1, libceed.NOTRANSPOSE, libceed.EVAL_INTERP, U, V);
+  b.apply(1, libceed.NOTRANSPOSE, libceed.EVAL_INTERP, U, V)
