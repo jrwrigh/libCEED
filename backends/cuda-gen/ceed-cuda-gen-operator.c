5275d2e18aa1e180f987454915d0d11cde31798e
jeremylt <jeremy.thompson@colorado.edu>
2019-11-25 16:22:55 -0700

    CUDA - drop AssembleLinearQF does not impl error messages

--- backends/cuda-gen/ceed-cuda-gen-operator.c
+++ backends/cuda-gen/ceed-cuda-gen-operator.c
@@ -157,13 +157,6 @@ static int CeedOperatorApplyAdd_Cuda_gen(CeedOperator op, CeedVector invec,
   return 0;
 }
 
-static int CeedOperatorAssembleLinearQFunction_Cuda(CeedOperator op) {
-  int ierr;
-  Ceed ceed;
-  ierr = CeedOperatorGetCeed(op, &ceed); CeedChk(ierr);
-  return CeedError(ceed, 1, "Backend does not implement QFunction assembly");
-}
-
 int CeedOperatorCreate_Cuda_gen(CeedOperator op) {
   int ierr;
   Ceed ceed;
@@ -173,9 +166,6 @@ int CeedOperatorCreate_Cuda_gen(CeedOperator op) {
   ierr = CeedCalloc(1, &impl); CeedChk(ierr);
   ierr = CeedOperatorSetData(op, (void *)&impl);
 
-  ierr = CeedSetBackendFunction(ceed, "Operator", op, "AssembleLinearQFunction",
-                                CeedOperatorAssembleLinearQFunction_Cuda);
-  CeedChk(ierr);
   ierr = CeedSetBackendFunction(ceed, "Operator", op, "ApplyAdd",
                                 CeedOperatorApplyAdd_Cuda_gen); CeedChk(ierr);
   ierr = CeedSetBackendFunction(ceed, "Operator", op, "Destroy",
